71c30d5e18d6716094e7385465ec0a680c0d88d2
sunhaaaaa <sunha219@naver.com>
2025-08-11 02:47:56 +0900

    ✨ Feat: JungleTalk step 상태에 따라 Header 조건부 렌더링 추가

--- src/shared/layouts/RootLayout.jsx
+++ src/shared/layouts/RootLayout.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Header from "../header/Header";
+import { useJungleTalkStore } from "../../store/jungleTalkStore";
 
 export default function RootLayout() {
-  return (
+  const { step } = useJungleTalkStore();
+
+return (
     <>
-      <Header />
+      {/* step이 3이면 헤더 숨김 */}
+      {!(window.location.pathname.includes("/jungletalk") && step === 3) && <Header />}
       <Outlet />
     </>
   );
