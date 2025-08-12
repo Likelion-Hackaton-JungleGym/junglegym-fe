00fbf31d491ef366deea1c5d21d6bce20d6f017c
Seulgi Han <chiaki2c@skuniv.ac.kr>
2025-08-13 00:13:34 +0900

    정글의 소리 Wrapper 태그 추가

--- src/pages/junglesound/JungleSoundPage.jsx
+++ src/pages/junglesound/JungleSoundPage.jsx
@@ -1,7 +1,13 @@
+import styled from "styled-components";
+
 export default function JungleSoundPage() {
   return (
-    <>
+    <Wrapper>
       <p>정글의 소리</p>
-    </>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  margin: 10px;
+`;
