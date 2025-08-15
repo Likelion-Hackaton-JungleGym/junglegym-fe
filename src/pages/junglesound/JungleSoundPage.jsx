175d1b268bf3f8a8fcbfd5902877a1239628a258
Seulgi Han <chiaki2c@skuniv.ac.kr>
2025-08-16 00:29:06 +0900

    정글사운드 기본 구현, 뉴스레터 5개 객체로 정리해서 사용

--- src/pages/junglesound/JungleSoundPage.jsx
+++ src/pages/junglesound/JungleSoundPage.jsx
@@ -1,13 +1,66 @@
 import styled from "styled-components";
+import NewsLetters from "./components/Newsletter.jsx";
 
 export default function JungleSoundPage() {
   return (
     <Wrapper>
-      <p>정글의 소리</p>
+      <HighlightText>
+        정글의 소리
+        <Highlight />
+      </HighlightText>
+      <SubText>
+        같은 정책을 우리 동네 관점으로 해석하고,
+        <br /> 지역 차이를 비교·질문하는 뉴스레터
+      </SubText>
+      <NewsLettersWrapper>
+        <NewsLetters id="newsletter1" />
+        <NewsLetters id="newsletter2" />
+        <NewsLetters id="newsletter3" />
+        <NewsLetters id="newsletter4" />
+        <NewsLetters id="newsletter5" />
+      </NewsLettersWrapper>
     </Wrapper>
   );
 }
 
 const Wrapper = styled.div`
-  margin: 10px;
+  margin: 20px;
+`;
+
+const HighlightText = styled.div`
+  position: relative;
+  margin: 5px;
+  color: #111111;
+  text-align: center;
+  font-size: 27px;
+  font-style: normal;
+  font-weight: 700;
+  padding-top: 20px;
+  letter-spacing: -1px;
+`;
+
+const Highlight = styled.div`
+  position: absolute;
+  bottom: 3px;
+  left: 50%;
+  width: 135px;
+  height: 13px;
+  background: #e1e0ff;
+  z-index: -1;
+  border-radius: 2px;
+  transform: translateX(-50%);
+`;
+
+const SubText = styled.p`
+  color: #111111;
+  text-align: center;
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 500;
+  letter-spacing: -0.3px;
+  margin-bottom: 30px;
+`;
+
+const NewsLettersWrapper = styled.div`
+  margin: 10px 0px;
 `;
