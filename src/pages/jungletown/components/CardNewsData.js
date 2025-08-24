f5026f886ad8b3df84e6b1a49a12cac3bf793a79
Seulgi Han <chiaki2c@skuniv.ac.kr>
2025-08-24 13:13:29 +0900

    최적화2

--- src/pages/jungletown/components/CardNewsData.js
+++ src/pages/jungletown/components/CardNewsData.js
@@ -1,3 +1,8 @@
+const BASE = import.meta.env.BASE_URL; // 루트 배포면 "/"
+const VER = import.meta.env.VITE_ASSET_VER || ""; // 캐시 버스터(선택)
+const v = VER ? `?v=${VER}` : "";
+const img = (name) => `${BASE}images/${name}${v}`;
+
 import CardNews1 from "./img/CardNews1.svg?url";
 import CardNews2 from "./img/CardNews2.svg?url";
 import CardNews3 from "./img/CardNews3.svg?url";
@@ -5,18 +10,18 @@ import CardNews4 from "./img/CardNews4.svg?url";
 import CardNews5 from "./img/CardNews5.svg?url";
 import CardNews6 from "./img/CardNews6.svg?url";
 
-const pol1 = "/images/pol1.svg";
-const pol2 = "/images/pol2.svg";
-const eco1 = "/images/eco1.svg";
-const eco2 = "/images/eco2.svg";
-const soc1 = "/images/soc1.svg";
-const soc2 = "/images/soc2.svg";
-const world1 = "/images/world1.svg";
-const world2 = "/images/world2.svg";
-const cul1 = "/images/cul1.svg";
-const cul2 = "/images/cul2.svg";
-const it1 = "/images/it1.svg";
-const it2 = "/images/it2.svg";
+const pol1 = img("pol1.svg");
+const pol2 = img("pol2.svg");
+const eco1 = img("eco1.svg");
+const eco2 = img("eco2.svg");
+const soc1 = img("soc1.svg");
+const soc2 = img("soc2.svg");
+const world1 = img("world1.svg");
+const world2 = img("world2.svg");
+const cul1 = img("cul1.svg");
+const cul2 = img("cul2.svg");
+const it1 = img("it1.svg");
+const it2 = img("it2.svg");
 
 export const ICON_MAP = {
   정치: [pol1, pol2],
