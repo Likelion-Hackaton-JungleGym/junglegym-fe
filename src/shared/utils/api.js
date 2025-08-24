2a4a25a17547cae728f63a2b0fe4b938808784e1
sunhaaaaa <sunha219@naver.com>
2025-08-25 05:06:58 +0900

    fix: api 프록시 해제

--- src/shared/utils/api.js
+++ src/shared/utils/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "",
+  baseURL: import.meta.env.PROD 
+    ? "https://api.junglegym.kr" 
+    : "", // 개발 환경에서는 프록시 사용
   timeout: 30 * 1000, // 타임아웃 30초
   withCredentials: false, // 백엔드에서 토큰 관련 이야기가 나오면 먼저 멘토에게 연락주세요.
 });
