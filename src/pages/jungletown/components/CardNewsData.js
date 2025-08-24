e92771781893f84186e4ca5cc7fda629c6e000c5
Seulgi Han <chiaki2c@skuniv.ac.kr>
2025-08-24 13:00:25 +0900

    카드뉴스 아이콘 이미지 최적화

--- src/pages/jungletown/components/CardNewsData.js
+++ src/pages/jungletown/components/CardNewsData.js
@@ -5,18 +5,18 @@ import CardNews4 from "./img/CardNews4.svg?url";
 import CardNews5 from "./img/CardNews5.svg?url";
 import CardNews6 from "./img/CardNews6.svg?url";
 
-import pol1 from "./img/pol1.svg?url";
-import pol2 from "./img/pol2.svg?url";
-import eco1 from "./img/eco1.svg?url";
-import eco2 from "./img/eco2.svg?url";
-import soc1 from "./img/soc1.svg?url";
-import soc2 from "./img/soc2.svg?url";
-import world1 from "./img/world1.svg?url";
-import world2 from "./img/world2.svg?url";
-import cul1 from "./img/cul1.svg?url";
-import cul2 from "./img/cul2.svg?url";
-import it1 from "./img/it1.svg?url";
-import it2 from "./img/it2.svg?url";
+const pol1 = "/images/pol1.svg";
+const pol2 = "/images/pol2.svg";
+const eco1 = "/images/eco1.svg";
+const eco2 = "/images/eco2.svg";
+const soc1 = "/images/soc1.svg";
+const soc2 = "/images/soc2.svg";
+const world1 = "/images/world1.svg";
+const world2 = "/images/world2.svg";
+const cul1 = "/images/cul1.svg";
+const cul2 = "/images/cul2.svg";
+const it1 = "/images/it1.svg";
+const it2 = "/images/it2.svg";
 
 export const ICON_MAP = {
   정치: [pol1, pol2],
