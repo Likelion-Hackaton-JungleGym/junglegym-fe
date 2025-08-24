a249f4f518efa02d5c5888efbb0743445d9d2caa
Seulgi Han <chiaki2c@skuniv.ac.kr>
2025-08-24 12:21:59 +0900

    안 쓰는 사진 삭제, 카드 이미지 ?url 추가

--- src/pages/jungletown/components/CardNewsData.js
+++ src/pages/jungletown/components/CardNewsData.js
@@ -1,28 +1,22 @@
-import CardNews1 from "./img/CardNews1.svg";
-import CardNews2 from "./img/CardNews2.svg";
-import CardNews3 from "./img/CardNews3.svg";
-import CardNews4 from "./img/CardNews4.svg";
-import CardNews5 from "./img/CardNews5.svg";
-import CardNews6 from "./img/CardNews6.svg";
+import CardNews1 from "./img/CardNews1.svg?url";
+import CardNews2 from "./img/CardNews2.svg?url";
+import CardNews3 from "./img/CardNews3.svg?url";
+import CardNews4 from "./img/CardNews4.svg?url";
+import CardNews5 from "./img/CardNews5.svg?url";
+import CardNews6 from "./img/CardNews6.svg?url";
 
-import graph1 from "./img/graph1.svg";
-import graph2 from "./img/graph1.svg";
-import graph3 from "./img/graph1.svg";
-import graph4 from "./img/graph1.svg";
-import graph5 from "./img/graph1.svg";
-
-import pol1 from "./img/pol1.svg";
-import pol2 from "./img/pol2.svg";
-import eco1 from "./img/eco1.svg";
-import eco2 from "./img/eco2.svg";
-import soc1 from "./img/soc1.svg";
-import soc2 from "./img/soc2.svg";
-import world1 from "./img/world1.svg";
-import world2 from "./img/world2.svg";
-import cul1 from "./img/cul1.svg";
-import cul2 from "./img/cul2.svg";
-import it1 from "./img/it1.svg";
-import it2 from "./img/it2.svg";
+import pol1 from "./img/pol1.svg?url";
+import pol2 from "./img/pol2.svg?url";
+import eco1 from "./img/eco1.svg?url";
+import eco2 from "./img/eco2.svg?url";
+import soc1 from "./img/soc1.svg?url";
+import soc2 from "./img/soc2.svg?url";
+import world1 from "./img/world1.svg?url";
+import world2 from "./img/world2.svg?url";
+import cul1 from "./img/cul1.svg?url";
+import cul2 from "./img/cul2.svg?url";
+import it1 from "./img/it1.svg?url";
+import it2 from "./img/it2.svg?url";
 
 export const ICON_MAP = {
   정치: [pol1, pol2],
@@ -46,30 +40,25 @@ export const CARDNEWS = [
     id: 1,
     card: CardNews1,
     newsCategory: "정치",
-    mediaImgUrl: graph1,
   },
   {
     id: 2,
     card: CardNews2,
     newsCategory: "경제",
-    mediaImgUrl: graph2,
   },
   {
     id: 3,
     card: CardNews3,
     newsCategory: "사회",
-    mediaImgUrl: graph3,
   },
   {
     id: 4,
     card: CardNews4,
     newsCategory: "세계",
-    mediaImgUrl: graph4,
   },
   {
     id: 5,
     card: CardNews5,
     newsCategory: "생활/문화",
-    mediaImgUrl: graph5,
   },
 ];
